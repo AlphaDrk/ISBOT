9c1d230edebc5fd09747c7e2a59fe16e48124404
AlphaDrk <nassiim.farhat@gmail.com>
2025-05-11 00:44:41 +0100

    commit 6

--- wsgi.py
+++ wsgi.py
@@ -1,11 +1,4 @@
 from app import app
 
-# This file serves as the entry point for Gunicorn
-# No need to run the app here as Gunicorn will handle that
-
 if __name__ == '__main__':
-    # This block is only for local development
-    # and won't be used in production with Gunicorn
-    import os
-    port = int(os.getenv('PORT', 8080))
-    app.run(host='0.0.0.0', port=port)
+    app.run()
