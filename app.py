dce032c7a8e730ab6027418c2595e6c020c7bb98
AlphaDrk <nassiim.farhat@gmail.com>
2025-05-11 11:46:02 +0100

    commit 13

--- app.py
+++ app.py
@@ -473,4 +473,5 @@ def view_shared_chat(token):
     return render_template('shared_chat.html', chat_history=processed_history)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port)
